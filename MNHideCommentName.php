9a7a66bad8e222a44270c5fdf4c45c84449de869
Moritz Naczenski <mnaczenski@users.noreply.github.com>
2018-01-23 07:22:39 +0100

    change codestyle

--- MNHideCommentName.php
+++ MNHideCommentName.php
@@ -12,20 +12,20 @@ class MNHideCommentName extends \Shopware\Components\Plugin
 		$context->scheduleClearCache(ActivateContext::CACHE_LIST_DEFAULT);
 	}
 
-  public static function getSubscribedEvents()
-  {
-  	return [
-    	'Enlight_Controller_Action_PostDispatchSecure_Frontend' => 'onFrontendPostDispatch'
-    ];
-  }
+	public static function getSubscribedEvents()
+	{
+		return [
+			'Enlight_Controller_Action_PostDispatchSecure_Frontend' => 'onFrontendPostDispatch'
+	    	];
+	}
 
-  public function onFrontendPostDispatch(\Enlight_Controller_ActionEventArgs $args)
-  {
-	  /** @var \Enlight_Controller_Action $controller */
-	  $controller = $args->get('subject');
-	  $view = $controller->View();
-	  $this->container->get('Template')->addTemplateDir(
-	  $this->getPath() . '/Resources/views/'
-	  );
-  }
+	public function onFrontendPostDispatch(\Enlight_Controller_ActionEventArgs $args)
+	{
+		 /** @var \Enlight_Controller_Action $controller */
+		  $controller = $args->get('subject');
+		  $view = $controller->View();
+		  $this->container->get('Template')->addTemplateDir(
+		  $this->getPath() . '/Resources/views/'
+		  );
+	}
 }
